f9dac8fd67b306a6b19e5f4f81038c1c15195c45
Hiroshi Noji <h.nouji@gmail.com>
2017-01-11 20:26:44 +0900

    Modify script

--- script/run_stanford_tagger.py
+++ script/run_stanford_tagger.py
@@ -2,8 +2,9 @@
 
 import sys, os, subprocess
 import StringIO
+import argparse
 
-CORENLP = '/Users/noji/Dropbox/tmp/stanford-corenlp-full-2015-12-09/*'
+CORENLP = '/Users/noji/Dropbox/tmp/stanford-corenlp-full-2015-12-09'
 # MODEL = 'edu/stanford/nlp/models/pos-tagger/english-left3words/english-left3words-distsim.tagger'
 MODEL = 'edu/stanford/nlp/models/pos-tagger/english-bidirectional/english-bidirectional-distsim.tagger'
 
@@ -23,8 +24,17 @@ def listify_tab_separated_file(input):
         sentences.append(sentence)
     return sentences
 
-def run_tagger(raw_sentences):
-    cmd = 'java -cp %s edu.stanford.nlp.tagger.maxent.MaxentTagger -model %s -outputFormat tsv -sentenceDelimiter newline -tokenize false' % (CORENLP, MODEL)
+def run_tagger(raw_sentences, corenlp, model):
+    if not corenlp:
+        corenlp = CORENLP
+    if corenlp[-1] == '/':
+        corenlp = corenlp[:-1]
+    corenlp = corenlp + '/*'
+
+    if not model:
+        model = MODEL
+
+    cmd = 'java -cp %s edu.stanford.nlp.tagger.maxent.MaxentTagger -model %s -outputFormat tsv -sentenceDelimiter newline -tokenize false' % (corenlp, model)
 
     p = subprocess.Popen(cmd.split(' '),
                          stdin=subprocess.PIPE, stdout=subprocess.PIPE)
@@ -58,11 +68,11 @@ def modify_tags(sentences, tagger_output):
     return [mod_sentence(s, tagger_s)
             for (s, tagger_s) in zip(sentences, tagger_sentences)]
 
-def run(input):
+def run(input, corenlp=None, model=None):
     sentences = read_conllx(input)
     raw_sentences = conll_to_raw(sentences)
 
-    tagger_output = run_tagger(raw_sentences)
+    tagger_output = run_tagger(raw_sentences, corenlp, model)
     mod_sentences = modify_tags(sentences, tagger_output)
 
     return mod_sentences
@@ -75,5 +85,11 @@ def write_conllx(sentences, output):
     output.flush()
 
 if __name__ == '__main__':
-    sentences = run(sys.stdin)
+    parser = argparse.ArgumentParser(description='Run stanford tagger on CoNLL input.')
+    parser.add_argument('--corenlp', default=CORENLP, help='Path to corenlp directory')
+    parser.add_argument('--model', default=MODEL, help='Model name')
+
+    args = parser.parse_args()
+
+    sentences = run(sys.stdin, args.corenlp, args.model)
     write_conllx(sentences, sys.stdout)
